9cbb8d091f00095072fe5ed55a43f56a703cfe5f
tuanngoc2004 <anninhthaiduong@gmail.com>
2024-04-30 15:17:41 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -89,4 +89,4 @@ Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show')
 Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-Route::get('/tasks/export-excel', [TaskController::class, 'exportExcel'])->name('tasks.exportExcel');
+// Route::get('/tasks/export-excel', [TaskController::class, 'exportExcel'])->name('tasks.exportExcel');
